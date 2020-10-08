be2cdf737aed6bc5e789b581adfe424dc91152dd
pritchardn <21726929@student.uwa.edu.au>
2020-10-08 18:23:07 +0800

    Updates method to load test cases

--- daliuge-common/dlg/common/reproducibility/test/test_drop_accumulate.py
+++ daliuge-common/dlg/common/reproducibility/test/test_drop_accumulate.py
@@ -1,190 +1,117 @@
 import unittest
-
+import json
 from dlg.common.reproducibility.constants import ReproducibilityFlags
 from dlg.common.reproducibility.reproducibility import accumulate_lgt_drop_data
+
+
+def load_drop(drop, level):
+    drop_dict = drop[level]
+    out_dict = {ReproducibilityFlags.RERUN: drop[level + "_rr"], ReproducibilityFlags.REPEAT: drop["lgt_rt"],
+                ReproducibilityFlags.REPRODUCE: drop[level + "_rp"],
+                ReproducibilityFlags.REPLICATE_SCI: drop[level + "_rpl_sci"],
+                ReproducibilityFlags.REPLICATE_COMP: drop[level + "_rpl_comp"]}
+    return drop_dict, out_dict
 
 
 class TestLGTDropAccumulate(unittest.TestCase):
 
     def test_accumulate_lgt_application(self):
-        self.lgt_a = {
-            "category": "BashShellApp",
-            "categoryType": "Application",
-            "fields": [
-                {
-                    "name": "Arg01",
-                    "text": "Arg01",
-                    "value": "echo -en 'Hello world' > %o0"
-                }
-            ],
-            "inputPorts": [],
-            "outputPorts": [
-                {
-                    "Id": "3o1",
-                    "IdText": "event"
-                }
-            ],
-            "streaming": True,
-        }
-        self.lgt_a_out = {
-            ReproducibilityFlags.RERUN: {'category_type': "Application",
-                                         'category': "BashShellApp",
-                                         'numInputPorts': 0,
-                                         'numOutputPorts': 1,
-                                         'streaming': True},
-            ReproducibilityFlags.REPEAT: {'category_type': "Application",
-                                          'category': "BashShellApp",
-                                          'numInputPorts': 0,
-                                          'numOutputPorts': 1,
-                                          'streaming': True},
-            ReproducibilityFlags.REPRODUCE: {'category_type': "Application",
-                                             'category': "BashShellApp"},
-            ReproducibilityFlags.REPLICATE_SCI: {'category_type': "Application",
-                                                 'category': "BashShellApp",
-                                                 'numInputPorts': 0,
-                                                 'numOutputPorts': 1,
-                                                 'streaming': True},
-            ReproducibilityFlags.REPLICATE_COMP: {'category_type': "Application",
-                                                  'category': "BashShellApp",
-                                                  'numInputPorts': 0,
-                                                  'numOutputPorts': 1,
-                                                  'streaming': True}
-        }
-        for flag, expected in self.lgt_a_out.items():
-            actual = accumulate_lgt_drop_data(self.lgt_a, flag)
+        with open("./drops/lga.json", 'r') as handle:
+            drop = json.load(handle)
+        lga, lgt_a_out = load_drop(drop, "lgt")
+        for flag, expected in lgt_a_out.items():
+            actual = accumulate_lgt_drop_data(lga, flag)
             self.assertEqual(actual, expected)
 
     def test_accumulate_lgt_data(self):
-        self.lgt_d = {
-            "category": "File",
-            "categoryType": "Data",
-            "fields": [
-                {
-                    "name": "check_filepath_exists",
-                    "text": "Check file path exists",
-                    "value": "1"
-                },
-                {
-                    "name": "filepath",
-                    "text": "File path",
-                    "value": "result1.out"
-                },
-                {
-                    "name": "dirname",
-                    "text": "Directory name",
-                    "value": "/home/data"
-                }
-            ],
-            "inputPorts": [
-                {
-                    "Id": "2i1",
-                    "IdText": "event"
-                }
-            ],
-            "isData": True,
-            "isGroup": False,
-            "outputPorts": [],
-            "streaming": False,
-        }
-        self.lgt_d_out = {
-            ReproducibilityFlags.RERUN: {'category_type': "Data",
-                                         'category': "File",
-                                         'numInputPorts': 1,
-                                         'numOutputPorts': 0,
-                                         'streaming': False},
-            ReproducibilityFlags.REPEAT: {'category_type': "Data",
-                                          'category': "File",
-                                          'numInputPorts': 1,
-                                          'numOutputPorts': 0,
-                                          'streaming': False},
-            ReproducibilityFlags.REPRODUCE: {'category_type': "Data",
-                                             'category': "File"},
-            ReproducibilityFlags.REPLICATE_SCI: {'category_type': "Data",
-                                                 'category': "File",
-                                                 'numInputPorts': 1,
-                                                 'numOutputPorts': 0,
-                                                 'streaming': False},
-            ReproducibilityFlags.REPLICATE_COMP: {'category_type': "Data",
-                                                  'category': "File",
-                                                  'numInputPorts': 1,
-                                                  'numOutputPorts': 0,
-                                                  'streaming': False}
-        }
-        for flag, expected in self.lgt_d_out.items():
-            actual = accumulate_lgt_drop_data(self.lgt_d, flag)
+        with open("./drops/lgd.json", 'r') as handle:
+            drop = json.load(handle)
+        lgd, lgt_d_out = load_drop(drop, "lgt")
+        for flag, expected in lgt_d_out.items():
+            actual = accumulate_lgt_drop_data(lgd, flag)
             self.assertEqual(actual, expected)
 
     def test_accumulate_lgt_group(self):
-        self.lgt_g = {
-            "category": "Scatter",
-            "categoryType": "Group",
-            "exitAppName": "",
-            "fields": [
-                {
-                    "name": "num_of_copies",
-                    "text": "Number of copies",
-                    "value": "4"
-                },
-                {
-                    "name": "scatter_axis",
-                    "text": "Scatter axis",
-                    "value": "time"
-                }
-            ],
-            "inputLocalPorts": [
-                {
-                    "Id": "88f46618-57ac-4e9c-99a7-f9cf2d682852",
-                    "IdText": "event"
-                }
-            ],
-            "inputPorts": [
-                {
-                    "Id": "b8cc69ef-de0c-4c6a-bba2-df12aff17a8b",
-                    "IdText": "event"
-                }
-            ],
-            "isData": False,
-            "isGroup": True,
-            "outputLocalPorts": [
-                {
-                    "Id": "c29554bd-8003-410a-9cce-c0d4376bb1ef",
-                    "IdText": "event"
-                }
-            ],
-            "outputPorts": [
-                {
-                    "Id": "a8e147cb-065c-49ee-8c52-25c53cf36a07",
-                    "IdText": "event"
-                }
-            ],
-            "streaming": False
-        }
-        self.lgt_g_out = {
-            ReproducibilityFlags.RERUN: {'category_type': "Group",
-                                         'category': "Scatter",
-                                         'numInputPorts': 1,
-                                         'numOutputPorts': 1,
-                                         'streaming': False},
-            ReproducibilityFlags.REPEAT: {'category_type': "Group",
-                                          'category': "Scatter",
-                                          'numInputPorts': 1,
-                                          'numOutputPorts': 1,
-                                          'streaming': False},
-            ReproducibilityFlags.REPRODUCE: {'category_type': "Group",
-                                             'category': "Scatter"},
-            ReproducibilityFlags.REPLICATE_SCI: {'category_type': "Group",
-                                                 'category': "Scatter",
-                                                 'numInputPorts': 1,
-                                                 'numOutputPorts': 1,
-                                                 'streaming': False},
-            ReproducibilityFlags.REPLICATE_COMP: {'category_type': "Group",
-                                                  'category': "Scatter",
-                                                  'numInputPorts': 1,
-                                                  'numOutputPorts': 1,
-                                                  'streaming': False}
-        }
-        for flag, expected in self.lgt_g_out.items():
-            actual = accumulate_lgt_drop_data(self.lgt_g, flag)
+        with open("./drops/lgg.json", 'r') as handle:
+            drop = json.load(handle)
+        lgg, lgt_g_out = load_drop(drop, "lgt")
+        for flag, expected in lgt_g_out.items():
+            actual = accumulate_lgt_drop_data(lgg, flag)
+            self.assertEqual(actual, expected)
+
+
+class TestLGDropAccumulate(unittest.TestCase):
+
+    def test_accumulate_lg_application(self):
+        with open("./drops/lga.json", 'r') as handle:
+            drop = json.load(handle)
+        lga, lgt_a_out = load_drop(drop, "lg")
+        for flag, expected in lgt_a_out.items():
+            actual = accumulate_lgt_drop_data(lga, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_lg_data(self):
+        with open("./drops/lgd.json", 'r') as handle:
+            drop = json.load(handle)
+        lgd, lgt_d_out = load_drop(drop, "lg")
+        for flag, expected in lgt_d_out.items():
+            actual = accumulate_lgt_drop_data(lgd, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_lg_group(self):
+        with open("./drops/lgg.json", 'r') as handle:
+            drop = json.load(handle)
+        lgg, lgt_g_out = load_drop(drop, "lg")
+        for flag, expected in lgt_g_out.items():
+            actual = accumulate_lgt_drop_data(lgg, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_pgt_application(self):
+        with open("./drops/lga.json", 'r') as handle:
+            drop = json.load(handle)
+        lga, lgt_a_out = load_drop(drop, "pgt")
+        for flag, expected in lgt_a_out.items():
+            actual = accumulate_lgt_drop_data(lga, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_pgt_data(self):
+        with open("./drops/lgd.json", 'r') as handle:
+            drop = json.load(handle)
+        lgd, lgt_d_out = load_drop(drop, "pgt")
+        for flag, expected in lgt_d_out.items():
+            actual = accumulate_lgt_drop_data(lgd, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_pgt_group(self):
+        with open("./drops/lgg.json", 'r') as handle:
+            drop = json.load(handle)
+        lgg, lgt_g_out = load_drop(drop, "pgt")
+        for flag, expected in lgt_g_out.items():
+            actual = accumulate_lgt_drop_data(lgg, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_pg_application(self):
+        with open("./drops/lga.json", 'r') as handle:
+            drop = json.load(handle)
+        lga, lgt_a_out = load_drop(drop, "pg")
+        for flag, expected in lgt_a_out.items():
+            actual = accumulate_lgt_drop_data(lga, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_pg_data(self):
+        with open("./drops/lgd.json", 'r') as handle:
+            drop = json.load(handle)
+        lgd, lgt_d_out = load_drop(drop, "pg")
+        for flag, expected in lgt_d_out.items():
+            actual = accumulate_lgt_drop_data(lgd, flag)
+            self.assertEqual(actual, expected)
+
+    def test_accumulate_pg_group(self):
+        with open("./drops/lgg.json", 'r') as handle:
+            drop = json.load(handle)
+        lgg, lgt_g_out = load_drop(drop, "pg")
+        for flag, expected in lgt_g_out.items():
+            actual = accumulate_lgt_drop_data(lgg, flag)
             self.assertEqual(actual, expected)
 
 
