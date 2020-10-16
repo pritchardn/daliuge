bfdbd6da08861fa3f28efa2802344d431fe17784
pritchardn <21726929@student.uwa.edu.au>
2020-10-16 11:39:27 +0800

    Adds pg buildblockdag test

--- daliuge-common/dlg/common/reproducibility/test/test_buildblockdag.py
+++ daliuge-common/dlg/common/reproducibility/test/test_buildblockdag.py
@@ -4,8 +4,9 @@ import unittest
 
 from dlg.common.reproducibility.constants import ReproducibilityFlags
 from dlg.common.reproducibility.reproducibility import init_lgt_repro_data, init_lg_repro_data, \
-    init_pgt_unroll_repro_drop_data, init_pgt_partition_repro_drop_data, build_blockdag, init_pgt_unroll_repro_data
-from dlg.dropmake.pg_generator import unroll, partition
+    init_pgt_unroll_repro_drop_data, init_pgt_partition_repro_drop_data, build_blockdag, init_pgt_unroll_repro_data, \
+    init_pg_repro_drop_data, init_pgt_partition_repro_data
+from dlg.dropmake.pg_generator import unroll, partition, resource_map
 
 
 def load_graph(graph):
@@ -37,6 +38,25 @@ def process_graph_partition(graph, level: ReproducibilityFlags):
         init_pgt_partition_repro_drop_data(drop)
     sigs, visited = build_blockdag(pgt, 'pgt')
     pgt.append(reprodata)
+    return sigs, visited
+
+
+def process_graph_pg(graph, level: ReproducibilityFlags):
+    lgt = init_lgt_repro_data(graph, str(level.value))
+    lg = init_lg_repro_data(lgt)
+    pgt = unroll(lg, oid_prefix='1')
+    init_pgt_unroll_repro_data(pgt)
+    reprodata = pgt.pop()
+    pgt = partition(pgt, algo='metis')
+    pgt.append(reprodata)
+    pgt = init_pgt_partition_repro_data(pgt)
+    nodes = ['127.0.0.1', '127.0.0.1']
+    reprodata = pgt.pop()
+    pg = resource_map(pgt, nodes, num_islands=1)
+    for drop in pg:
+        init_pg_repro_drop_data(drop)
+    sigs, visited = build_blockdag(pg, 'pg')
+    pg.append(reprodata)
     return sigs, visited
 
 
@@ -120,9 +140,9 @@ class TestPGTPartitionBuildBlockDAG(unittest.TestCase):
     def test_cycle(self):
         graph = load_graph('./graphs/cycle.graph')
         with self.assertRaises(Exception):
-            process_graph(graph, ReproducibilityFlags.RERUN)
+            process_graph_partition(graph, ReproducibilityFlags.RERUN)
         with self.assertRaises(Exception):
-            process_graph(graph, ReproducibilityFlags.REPRODUCE)
+            process_graph_partition(graph, ReproducibilityFlags.REPRODUCE)
 
     def test_split(self):
         lgt = load_graph('./graphs/split.graph')
@@ -148,9 +168,9 @@ class TestPGTPartitionBuildBlockDAG(unittest.TestCase):
     def test_bidirectional(self):
         lgt = load_graph('./graphs/bidirectional.graph')
         with self.assertRaises(Exception):
-            process_graph(lgt, ReproducibilityFlags.RERUN)
+            process_graph_partition(lgt, ReproducibilityFlags.RERUN)
         with self.assertRaises(Exception):
-            process_graph(lgt, ReproducibilityFlags.REPRODUCE)
+            process_graph_partition(lgt, ReproducibilityFlags.REPRODUCE)
 
     def test_scatter(self):
         lgt = load_graph('./graphs/scatter_test.graph')
@@ -159,6 +179,67 @@ class TestPGTPartitionBuildBlockDAG(unittest.TestCase):
         for flag in ReproducibilityFlags:
             graph = copy.deepcopy(lgt)
             sigs, visited = process_graph_partition(graph, flag)
+            self.assertEqual(expected_visited, visited)
+
+
+class TestPGBuildBlockDAG(unittest.TestCase):
+
+    def test_singlecomponent(self):
+        lgt = load_graph('./graphs/singlecomponent.graph')
+        expected_visited = ['1_-1_0']
+        for flag in ReproducibilityFlags:
+            sigs, visited = process_graph_pg(lgt, flag)
+            self.assertEqual(expected_visited, visited)
+
+    def test_chain(self):
+        lgt = load_graph('./graphs/chain.graph')
+        expected_visited = ['1_-1_0', '1_-2_0', '1_-3_0']
+        for flag in ReproducibilityFlags:
+            sigs, visited = process_graph_pg(lgt, flag)
+            self.assertEqual(expected_visited, visited)
+
+    def test_cycle(self):
+        graph = load_graph('./graphs/cycle.graph')
+        with self.assertRaises(Exception):
+            process_graph_pg(graph, ReproducibilityFlags.RERUN)
+        with self.assertRaises(Exception):
+            process_graph_pg(graph, ReproducibilityFlags.REPRODUCE)
+
+    def test_split(self):
+        lgt = load_graph('./graphs/split.graph')
+        expected_visited = ['1_-1_0', '1_-3_0', '1_-2_0']
+        for flag in ReproducibilityFlags:
+            sigs, visited = process_graph_pg(lgt, flag)
+            self.assertEqual(expected_visited, visited)
+
+    def test_transitive(self):
+        lgt = load_graph('./graphs/transitive.graph')
+        expected_visited = ['1_-1_0', '1_-2_0', '1_-3_0']
+        for flag in ReproducibilityFlags:
+            sigs, visited = process_graph_pg(lgt, flag)
+            self.assertEqual(expected_visited, visited)
+
+    def test_multichain(self):
+        lgt = load_graph('./graphs/multi-chain.graph')
+        expected_visited = ['1_-3_0', '1_-4_0', '1_-1_0', '1_-2_0']
+        for flag in ReproducibilityFlags:
+            sigs, visited = process_graph_pg(lgt, flag)
+            self.assertEqual(expected_visited, visited)
+
+    def test_bidirectional(self):
+        lgt = load_graph('./graphs/bidirectional.graph')
+        with self.assertRaises(Exception):
+            process_graph_pg(lgt, ReproducibilityFlags.RERUN)
+        with self.assertRaises(Exception):
+            process_graph_pg(lgt, ReproducibilityFlags.REPRODUCE)
+
+    def test_scatter(self):
+        lgt = load_graph('./graphs/scatter_test.graph')
+        expected_visited = ['1_-1_0', '1_-3_0/3', '1_-3_0/2', '1_-3_0/1', '1_-3_0/0', '1_-2_0/3', '1_-4_0/3',
+                            '1_-2_0/2', '1_-4_0/2', '1_-2_0/1', '1_-4_0/1', '1_-2_0/0', '1_-4_0/0', '1_-5_0']
+        for flag in ReproducibilityFlags:
+            graph = copy.deepcopy(lgt)
+            sigs, visited = process_graph_pg(graph, flag)
             self.assertEqual(expected_visited, visited)
 
 
